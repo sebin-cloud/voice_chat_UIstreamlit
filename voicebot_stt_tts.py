de0a7ce20180102c41b71575317f1d1449be8370
sebin-cloud <dreamworks4me@naver.com>
2024-08-02 12:29:26 +0900

    APi설정

--- voicebot_stt_tts.py
+++ voicebot_stt_tts.py
@@ -1,18 +1,18 @@
 # Streamlit 패키지 추가
-# pip install streamlit
-# streamlit 실행 : streamlit run app.py
 import streamlit as st
 
 ##### 메인 함수 #####
 def main():
-    # 페이지 타이틀 설정
-    st.set_page_config(page_title="음성 챗봇 프로그램", layout="wide")
+    # 기본 설정
+    st.set_page_config(
+        page_title="음성 챗봇 프로그램",
+        layout="wide")
 
     # 제목 
     st.header("음성 챗봇 프로그램")
 
     # 구분선
-    st.markdown("---")    
+    st.markdown("---")
 
     # 기본 설명
     with st.expander("음성 챗봇 프로그램에 관하여", expanded=True):
@@ -27,11 +27,72 @@ def main():
 
         st.markdown("")
 
+    # 사이드바 생성
+    with st.sidebar:
 
-if __name__ == "__main__" :
-    print()
-#실행함수
+        # GPT 모델을 선택하기 위한 라디오 버튼
+        model = st.radio(label="GPT 모델", options=["gpt-3.5-turbo", "gpt-4o", "gpt-4-turbo"])
+        st.markdown("---")
+
+        # 리셋 버튼 생성
+        if st.button(label="초기화"):
+            # 리셋 코드 
+            pass
+        
+if __name__=="__main__":
     main()
+
+#Open Ai 패키지 추가
+import openai
+import os
+from dotenv import load_dotenv
+
+#.env 파일 경로 지정 .env가 현대 dir이 아닐때 경로를 지정해주어야함
+load_dotenv()
+
+#Open AI API키 설정하기
+api_key = os.environ.get('OPEN_API_KEY')
+
+client = openai.OpenAI(api_key=api_key)
+
+####메인 함수 ####
+def main():
+    # 기본 설정
+    st.set_page_config(
+        page_title="음성 챗봇 프로그램",
+        layout="wide")
+
+    # 제목 
+    st.header("음성 챗봇 프로그램")
+
+    # 구분선
+    st.markdown("---")
+
+    # 기본 설명
+    with st.expander("음성 챗봇 프로그램에 관하여", expanded=True):
+        st.write(
+        """     
+        - 음성 번역 챗봇 프로그램의 UI는 스트림릿을 활용합니다.
+        - STT(Speech-To-Text)는 OpenAI의 Whisper를 활용합니다. 
+        - 답변은 OpenAI의 GPT 모델을 활용합니다. 
+        - TTS(Text-To-Speech)는 OpenAI의 TTS를 활용합니다.
+        """
+        )
+
+        st.markdown("")
+
+    # 사이드바 생성
+    with st.sidebar:
+
+        # GPT 모델을 선택하기 위한 라디오 버튼
+        model = st.radio(label="GPT 모델", options=["gpt-3.5-turbo", "gpt-4o", "gpt-4-turbo"])
+
+        st.markdown("---")
+
+# 리셋 버튼 생성
+        if st.button(label ="초기화"):
+            # 리셋 코드
+            pass
 
 
 
